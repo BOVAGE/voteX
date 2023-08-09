317239e6258a0301ea7e45f3c7518ae92e572130
bovage <fayemibolu2@gmail.com>
2023-08-09 12:19:26 +0100

    feat: add Voter model to admin

--- apps/voting/admin.py
+++ apps/voting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Voter
+
 
 # Register your models here.
+@admin.register(Voter)
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ["id", "election", "email", "is_verified"]
